84a5d19de2d1b9050a31e15fe77ca8d3a28491ef
Charlie <charlietech45@gmail.com>
2015-05-28 00:51:21 -0500

    Update index.elm

--- index.elm
+++ index.elm
@@ -1,3 +1,20 @@
-import Text(..)
+import Graphics.Element exposing (..)
 
-main = asText("Our main project")
+import Markdown
+main : Element
+main = Markdown.toElement """
+  # Welcome to the FPC Page
+  * Projects
+    - [This Website](https://github.com/ttu-fpclub/fpc-website/)
+    - [A Chemistry Aid](https://github.com/thepygoscelis/)
+    - More to come soon.. - Charlie
+  * Languages
+    - [Racket](http://racket-lang.org/)
+    - [R](http://www.r-project.org/)
+    - [Elm](http://elm-lang.org/)
+    - [Haskell](https://www.haskell.org/)
+    - [Common Lisp](https://common-lisp.net/)
+    - [Scheme](http://www.scheme.com/)
+    - [Elixir](http://elixir-lang.org/)
+    - [Erlang](http://www.erlang.org/)
+  """
